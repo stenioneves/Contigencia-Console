3c05bf6acac2ae658089647d10b0d968bb1c2518
Stenio Neves Silva <stenionevessilva@gmail.com>
2020-01-11 19:21:42 -0300

    Classe principal do sistema

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using contigencia.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using contigencia.UXGUI;
+namespace contigencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  /**
+        var dbContext = new FinanceiroContext();
+            var transacoes = dbContext.Transacao.ToList();
+            foreach (var t in transacoes)
+            {
+                Console.WriteLine($"Transação :{t.IdTransacao}-Tipo:{t.Tipo}-"+
+                $"Descrição:{t.Descricao}- Valor:{t.Valor}-Data:{t.Data}");
+                Console.WriteLine("\n");
+                Console.WriteLine("*******-------***************************************");
+            
+            }*/
+
+              Usuario us = new Usuario();
+              Login lg =new Login();
+             lg.TelaLogin();
+             
+              
+        }
+    }
+}
